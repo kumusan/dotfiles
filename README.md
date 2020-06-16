81a1ef771eddb4d3f939d6da1fcc6c7063f70312
kumu <kumu@kumunoMacBook-Air.local>
2020-06-16 16:24:40 +0900

    👻

---
+++ README.md
@@ -0,0 +1,13 @@
+chmod +x dotfilesInstaller.sh
+./dotfilesInstaller.sh
+
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+
+brew bundle install
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+ln -s ~/dotfiles/zsh/.zsh.prompts ~/.zsh.prompts
+ln -s ~/dotfiles/zsh/.zlogin ~/.zlogin
+ln -s ~/dotfiles/zsh/.zlogout ~/.zlogout
+ln -s ~/dotfiles/zsh/.zpreztorc ~/.zpreztorc
+ln -s ~/dotfiles/zsh/.zprofile ~/.zprofile
+ln -s ~/dotfiles/zsh/.zshenv ~/.zshenv
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.config ~/.config
+ln -s ~/dotfiles/.vim ~/.vim
