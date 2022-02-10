3aad2f3553f58882841660d04b152735cbe4427e
kumusan <ryogi0200@gmail.com>
2022-02-10 19:47:18 +0900

    👓

--- .hyper.js
+++ .hyper.js
@@ -16,18 +16,6 @@ module.exports = {
     fontFamily:
       '"Menlo", "ProFont for Powerline", "DejaVu Sans Mono", "Consolas", "Lucida Console", "monospace", "Droid Sans Nerd Font Complete", "Menlo Regular Nerd Font", "Menlo Nerd Font"',
 
-    // default font weight: 'normal' or 'bold'
-    fontWeight: "normal",
-
-    // font weight for bold characters: 'normal' or 'bold'
-    fontWeightBold: "bold",
-
-    // line height as a relative unit
-    lineHeight: 1,
-
-    // letter spacing as a relative unit
-    letterSpacing: 0,
-
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: "rgba(248,28,229,0.8)",
 
@@ -52,22 +40,7 @@ module.exports = {
 
     // border color (window, tabs)
     borderColor: "#333",
-
-    // custom CSS to embed in the main window
-    // css: `.terms_terms{background-image:url("file://Users/kumu/Downloads/jepg.jpeg");};`,
-
-    // custom CSS to embed in the terminal window
-    termCSS: "",
-
-    // if you're using a Linux setup which show native menus, set to false
-    // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: "",
-
-    // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: "",
-
+      
     // custom padding (CSS format, i.e.: `top right bottom left`)
     padding: "12px 14px",
 
@@ -93,20 +66,6 @@ module.exports = {
       lightWhite: "#FFFFFF"
     },
 
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //
-    // Windows
-    // - Make sure to use a full path if the binary name doesn't work
-    // - Remove `--login` in shellArgs
-    //
-    // Bash on Windows
-    // - Example: `C:\\Windows\\System32\\bash.exe`
-    //
-    // PowerShell on Windows
-    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: "",
-
     // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
     // by default `['--login']` will be used
     shellArgs: ["--login"],
@@ -117,29 +76,9 @@ module.exports = {
     // set to `false` for no bell
     bell: "SOUND",
 
-    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
-    copyOnSelect: true,
-
-    // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
-    defaultSSHApp: true,
-
     // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
     // selection is present (`true` by default on Windows and disables the context menu feature)
     quickEdit: false,
-
-    // choose either `'vertical'`, if you want the column mode when Option key is hold during selection (Default)
-    // or `'force'`, if you want to force selection regardless of whether the terminal is in mouse events mode
-    // (inside tmux or vim with mouse mode enabled for example).
-    macOptionSelectionMode: "vertical",
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
-
-    // Whether to use the WebGL renderer. Set it to false to use canvas-based
-    // rendering (slower, but supports transparent backgrounds)
-    webGLRenderer: true
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
   },
 
   // a list of plugins to fetch and install from npm
